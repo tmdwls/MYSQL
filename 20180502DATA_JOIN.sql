11de1d5816f6c1ef6a46a33f4c8cf56775afd21e
tmdwls <tmdwls0831@naver.com>
2018-05-08 11:25:21 +0900

    .

--- 20180502DATA_JOIN.sql
+++ 20180502DATA_JOIN.sql
@@ -1,21 +1,36 @@
-select 	a.name,
+#1.관리자인 회원의 id, name만 출력하세요#
+select a.id, 
+		 a.name 
+from member a, 
+	  auth b
+where a.authcode = b.authcode
+and authority = '회원';	
+####################################	
+
+
+#2.글제목, 회원이름 두가지만 선택하세요
+select 
+	a.title, 
+	b.name 
+from board a, member b
+where a.id = b.id;
+####################################
+
+
+#3.권한이 '회원' 회원이름, 회원id, 제목, 내용, 권한만 출력하세요
+select 	a.name, 
+			b.title,
 			a.id,
-			b.title, 
 			b.content, 
-			c.authority
+			c.authority 
 	from  member a, 
 			board b, 
 			auth c
 	where a.id = b.id
 	and a.authcode = c.authcode
-	and authority = '회원'
+	and authority = '회원';
+####################################	
+	
 
-
-select a.name, 
-		 a.authcode, 
-		 c.title, 
-		 c.content		 
-from member a, board c
-where a.id = c.id
 	
 	
